263839c54b4d069b73b97ac30e0f2520ad30c409
goltsman <goltsman@yandex-team.ru>
2019-04-15 11:53:48 +0300

    Fixes for chat usage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test-bot
+
+pip install pytelegrambotapi
--- app/handlers.py
+++ app/handlers.py
@@ -5,42 +5,42 @@ from app import bot, NAME, SURNAME, AGE
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.from_user.id, 'Привет! Я - тестовый бот. Поговори со мной.')
+    bot.send_message(message.chat.id, 'Привет! Я - тестовый бот. Поговори со мной.')
 
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
-    bot.send_message(message.from_user.id, 'Напиши /reg')
+    bot.send_message(message.chat.id, 'Напиши /reg')
 
 
 @bot.message_handler(commands=['reg'])
 def handle_reg(message):
-    bot.send_message(message.from_user.id, "Как тебя зовут?")
+    bot.send_message(message.chat.id, "Как тебя зовут?")
     bot.register_next_step_handler(message, get_name)  # следующий шаг – функция get_name
 
 
 # Handles all text messages that match the regular expression
 @bot.message_handler(content_types=['text'], regexp="python")
 def handle_python_message(message):
-    bot.send_message(message.from_user.id, "Я обожаю python!")
+    bot.send_message(message.chat.id, "Я обожаю python!")
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text_message(message):
-    bot.send_message(message.from_user.id, message.text)
+    bot.send_message(message.chat.id, message.text)
 
 
 def get_name(message):
     global NAME
     NAME = message.text
-    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.send_message(message.chat.id, 'Какая у тебя фамилия?')
     bot.register_next_step_handler(message, get_surname)
 
 
 def get_surname(message):
     global SURNAME
     SURNAME = message.text
-    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
+    bot.send_message(message.chat.id, 'Сколько тебе лет?')
     bot.register_next_step_handler(message, get_age)
 
 
@@ -49,14 +49,14 @@ def get_age(message):
     try:
         AGE = int(message.text)  # проверяем, что возраст введен корректно
     except (TypeError, ValueError):
-        bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+        bot.send_message(message.chat.id, 'Цифрами, пожалуйста')
     keyboard = telebot.types.InlineKeyboardMarkup()  # наша клавиатура
     key_yes = telebot.types.InlineKeyboardButton(text='Да', callback_data='yes')  # кнопка «Да»
     keyboard.add(key_yes)  # добавляем кнопку в клавиатуру
     key_no = telebot.types.InlineKeyboardButton(text='Нет', callback_data='no')
     keyboard.add(key_no)
     question = 'Тебе {age} лет, тебя зовут {name} {surname}?'.format(age=AGE, name=NAME, surname=SURNAME)
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+    bot.send_message(message.chat.id, text=question, reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
